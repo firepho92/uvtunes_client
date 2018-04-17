fc8fad9371d6018f6ee946b8f682a30ba564012b
Alex <auza920205@gmail.com>
2018-04-17 09:51:47 -0500

    login and signup fixed

--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -14,19 +14,14 @@ class LoginScreen extends Component {
             <h2>Entrar</h2>
             <form onSubmit={e => this.handleSubmit(e)}>
               <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
               </div>
               <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <label for="exampleInputPassword1">Contraseña</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
               </div>
-              <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Entrar</button>
             </form>
           </div>
         </div>
--- src/components/SignupScreen/SignupScreen.jsx
+++ src/components/SignupScreen/SignupScreen.jsx
@@ -10,19 +10,26 @@ class SignupScreen extends Component {
             <h2>Crear cuenta</h2>
             <form onSubmit={e => this.handleSubmit(e)}>
               <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
+                <label for="exampleNombre">Nombre(s)</label>
+                <input type="text" className="form-control" id="exampleNombre" aria-describedby="nombreHelp" placeholder="Nombre(s)" />
+              </div>
+              <div class="form-group">
+                <label for="exampleApellido">Apellidos</label>
+                <input type="text" className="form-control" id="exampleApellido" aria-describedby="apellidoHelp" placeholder="Apellidos" />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
               </div>
               <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <label for="exampleInputPassword1">Contraseña</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
               </div>
-              <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+              <div className="form-group">
+                <label for="exampleInputPassword2">Verificar contraseña</label>
+                <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Verficar contraseña" />
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Aceptar</button>
             </form>
           </div>
         </div>
